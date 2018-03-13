a0ce6cc6c0da1af9393ab0cac80531499e1a1ecf
wratterman <will.ratterman@gmail.com>
2018-03-12 21:22:40 -0600

    created first unit test for Make database model; should have a unique name to initialize. deleted_at to be default nil for now; next step created database model

---
+++ spec/models/make_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Make, type: :model do
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+    it { is_expected.to validate_presence_of(:deleted_at) }
+  end
+end
